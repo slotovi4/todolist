56c91b7c1e9a63666bcbc1b6cb0d4ed2d062a5c2
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-26 22:41:07 +0300

    added styles to PageHOC component

--- src/components/PageHOC/PageHOC.tsx
+++ src/components/PageHOC/PageHOC.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
+import { cn } from '@bem-react/classname';
 
 // components
 import { List } from '../containers';
 
-const PageHOC = (PassedComponent: any): JSX.Element => (
-  <div className="page">
-    <PassedComponent />
-    <List />
-  </div>
-);
+// styles
+import './PageHOC.scss';
 
+const PageHOC = (PassedComponent: any): JSX.Element => {
+  const page = cn('page');
+
+  return (
+    <div className={page()}>
+      <PassedComponent />
+      <List />
+    </div>
+  );
+};
 export default PageHOC;
