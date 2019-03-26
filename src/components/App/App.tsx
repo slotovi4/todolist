1501ae313fadd1ea278ba15fc9f458560a7bc539
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-26 22:00:39 +0300

    update App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,20 +8,16 @@ import store from '../../store';
 // components
 import { HomePage, CreatePage, EditPage } from '../containers';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route exact={true} path="/" component={HomePage} />
-            <Route exact={true} path="/create" component={CreatePage} />
-            <Route exact={true} path="/edit/:id" component={EditPage} />
-          </Switch>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/create" component={CreatePage} />
+        <Route exact={true} path="/edit/:id" component={EditPage} />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
 export default App;
