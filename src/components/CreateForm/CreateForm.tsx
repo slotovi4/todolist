71bd7bf5d4758429b953f641ccc44c757e762427
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-27 20:08:16 +0300

    added importance styles

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -27,52 +27,52 @@ class CreateForm extends React.Component<IProps, IState> {
 
   public render() {
     const create = cn('CreateForm');
-    const { title, text } = this.state;
+    const { importance } = this.state;
 
     return (
       <section className={create()}>
         <h1 className={create('Title')}>Create todo</h1>
+        {importance &&
+          importance.map((color, i) => (
+            <div
+              className={create('Importance', { color, disabled: true })}
+              key={`color_${i}`}
+            />
+          ))}
         <form action="" onSubmit={this.submit}>
           <label htmlFor="title">Title</label>
           <input
             type="text"
             id="title"
             required={true}
-            defaultValue={title}
             onChange={e => this.setState({ title: e.target.value.trim() })}
           />
           <label htmlFor="text">Text</label>
           <textarea
             id="text"
             required={true}
-            defaultValue={text}
             onChange={e => this.setState({ text: e.target.value.trim() })}
           />
           <span>Importance</span>
-          <label
+          <div
             className={create('Importance', { color: 'red' })}
-            htmlFor="red"
-            onClick={() => this.onImportance('red')}
+            onClick={e => this.onImportance(e, 'red')}
           />
-          <label
+          <div
             className={create('Importance', { color: 'green' })}
-            htmlFor="green"
-            onClick={() => this.onImportance('green')}
+            onClick={e => this.onImportance(e, 'green')}
           />
-          <label
+          <div
             className={create('Importance', { color: 'orange' })}
-            htmlFor="opange"
-            onClick={() => this.onImportance('orange')}
+            onClick={e => this.onImportance(e, 'orange')}
           />
-          <label
+          <div
             className={create('Importance', { color: 'purple' })}
-            htmlFor="purple"
-            onClick={() => this.onImportance('purple')}
+            onClick={e => this.onImportance(e, 'purple')}
           />
-          <label
+          <div
             className={create('Importance', { color: 'black' })}
-            htmlFor="black"
-            onClick={() => this.onImportance('black')}
+            onClick={e => this.onImportance(e, 'black')}
           />
           <button type="submit">create</button>
         </form>
@@ -80,16 +80,27 @@ class CreateForm extends React.Component<IProps, IState> {
     );
   }
 
-  private onImportance = (color: string) => {
+  // on click importance
+  private onImportance = (e: any, color: string) => {
     const arr: any = this.state.importance;
+    const button = e.target;
 
+    // save state
     if (arr.indexOf(color) === -1) {
       this.setState({ importance: [...arr, color] });
     } else {
       this.setState({ importance: arr.filter((el: string) => el !== color) });
     }
+
+    // change style
+    if (button.classList.contains('CreateForm-Importance_active')) {
+      button.classList.remove('CreateForm-Importance_active');
+    } else {
+      button.classList.add('CreateForm-Importance_active');
+    }
   };
 
+  // on submit
   private submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { createTodo } = this.props;
